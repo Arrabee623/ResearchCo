d162c397dc7e456d0051b05495a59007915af2ab
Arrabee623 <arrabee78@gmail.com>
2023-09-03 15:49:14 +0900

    2 Forms were added

--- App.py
+++ App.py
@@ -11,19 +11,31 @@ root.title('企業研究')
 root.geometry('800x600')
 
 # ウィジェットの作成
-frame = ttk.Frame(root, padding=10)
-frame.grid()
+frame1 = ttk.Frame(root, padding=10, relief="solid")
+frame1.grid()
 
 # 会社名
 labelName = ttk.Label(
-    frame, 
+    frame1, 
     text='会社名')
 labelName.grid(row=0, column=0, sticky=E)
 companyName = StringVar()
 entryName = ttk.Entry(
-    frame,
+    frame1,
     textvariable=companyName)
 entryName.grid(row=0, column=1)
+
+# 会社形態
+labelForm = ttk.Label(
+    frame1,
+    text='会社形態(株式会社/その他形態)')
+labelForm.grid(row=1, column=0)
+companyForm = StringVar()
+entryForm = ttk.Entry(
+    frame1,
+    textvariable=companyForm)
+entryForm.grid(row=1, column=1)
+
 
 # アプリケーションの実行
 root.mainloop()
