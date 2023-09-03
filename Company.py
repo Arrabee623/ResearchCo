19535f9a1af04fbe0d76be9b6401a786c14ede04
Arrabee623 <arrabee78@gmail.com>
2023-09-03 14:02:54 +0900

    Add classification(Company)

--- Company.py
+++ Company.py
@@ -17,3 +17,36 @@ class Company:
         self.establish = establish  # 設立年
         self.salary = salary    # 平均年収
         self.what = what    # 何をしている会社か(概要)
+    
+    # 特定のパラメータについて分類を行う
+    def classification(self):
+        # 売上規模
+        if self.income >= 10000:
+            self.salesScale = "[LL] 超大企業"
+        elif self.income >= 3000:
+            self.salesScale = "[L] 大企業"
+        elif self.income >= 500:
+            self.salesScale = "[M] 中堅企業"
+        else:
+            self.salesScale = "[S] 中小企業"
+        
+        # 従業員規模
+        if self.employees >= 5000:
+            self.employeeScale = "[LL]"
+        elif self.employees >= 1000:
+            self.employeeScale = "[L]"
+        elif self.employees >= 300:
+            self.employeeScale = "[M]"
+        else:
+            self.employeeScale = "[S]"
+        
+        # 平均年収の区分
+        if self.salary >= 800:
+            self.salaryScale = "高い"
+        elif self.salary > 500:
+            self.salaryScale = "平均的"
+        else:
+            self.salaryScale = "低い"
+
+
+        
